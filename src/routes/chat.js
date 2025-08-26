dc5f994b2a640457f3c1f535eb03eef15137f805
Tsion Birhanu <tsionbirhanu08@gmail.com>
2025-08-26 15:00:34 -0700

    fix: pool import destructuring

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const pool = require('../config/database');
+const { pool } = require('../config/database');
+
 const { authenticateToken } = require('../middleware/auth');
 
 const router = express.Router();
